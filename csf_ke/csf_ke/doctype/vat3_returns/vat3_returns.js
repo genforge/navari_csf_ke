fc2479aafeb0366bee0caf9f6519bfd3682c60af
emmanuel-mwendwa <mwendwae054@gmail.com>
2024-10-10 16:35:21 +0300

    feat: create new doctypes to handle vat3 vat3_returns
    VAT3 Returns
    VAT3 Returns Invoice (Child Doctype)
    
    Update the patch to add the is_filed check field

--- csf_ke/csf_ke/doctype/vat3_returns/vat3_returns.js
+++ csf_ke/csf_ke/doctype/vat3_returns/vat3_returns.js
@@ -1,8 +1,45 @@
 // Copyright (c) 2024, Navari Ltd and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("VAT3 Returns", {
-// 	refresh(frm) {
+ frappe.ui.form.on("VAT3 Returns", {
+ 	buying: function(frm) {
+ 	    if (frm.doc.buying) {
+ 	        frm.set_value('selling', 0);
+ 	    }
+ 	},
+ 	selling: function(frm) {
+ 	    if (frm.doc.selling) {
+ 	        frm.set_value('buying', 0);
+ 	    }
+ 	},
+    fetch_invoices: function(frm) {
+        frappe.call({
+            method: 'csf_ke.csf_ke.doctype.vat3_returns.vat3_returns.fetch_invoices',
+            args: {
+                buying: frm.doc.buying,
+                selling: frm.doc.selling,
+                from_date: frm.doc.from_date,
+                to_date: frm.doc.to_date,
+                company: frm.doc.company
+            },
+            callback: function(r) {
 
-// 	},
-// });
+                if (r.message) {
+
+                    frm.clear_table('vat3_returns_invoices');
+
+                    r.message.forEach(function(invoice) {
+
+                        let row = frm.add_child('vat3_returns_invoices');
+                        row.document_type = frm.doc.selling ? "Sales Invoice" : "Purchase Invoice";
+                        row.invoice_number = invoice.name;
+                        row.invoice_date = invoice.posting_date;
+                        row.invoice_amount = invoice.grand_total;
+                });
+                frm.refresh_field('vat3_returns_invoices');
+                }
+            }
+        });
+    },
+ });
+ 
--- csf_ke/csf_ke/doctype/vat3_returns/vat3_returns.py
+++ csf_ke/csf_ke/doctype/vat3_returns/vat3_returns.py
@@ -1,9 +1,38 @@
 # Copyright (c) 2024, Navari Ltd and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
+from frappe import _
 from frappe.model.document import Document
 
 
 class VAT3Returns(Document):
-	pass
+
+    def validate(self):
+        pass
+
+    def on_submit(self):
+        pass
+
+@frappe.whitelist()
+def fetch_invoices(buying, selling, from_date, to_date, company):
+
+    if buying == '1':
+        invoice_type = "Purchase Invoice"
+    elif selling == '1':
+        invoice_type = "Sales Invoice"
+
+    invoices =  frappe.get_all(
+        invoice_type,
+        filters={
+            "docstatus": 1,
+            "posting_date": ["between", [from_date, to_date]],
+            "company": company, 
+            "is_filed": 0
+        },
+        fields=["name", "grand_total"],
+    )
+
+    frappe.msgprint(str(invoices))
+
+    return invoices
