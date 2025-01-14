80f44f9545ac20951c6448d1a0c20866dafad250
emmanuel-mwendwa <mwendwae054@gmail.com>
2025-01-14 08:23:00 +0300

    (chore): rename custom fields and filters from hscode to tims_hscode

--- csf_ke/hooks.py
+++ csf_ke/hooks.py
@@ -30,7 +30,7 @@ fixtures = [
                     "Employee-tax_id",
                     "Salary Component-p9a_tax_deduction_card_type",
                     "Item Tax-custom_column_break",
-                    "Item Tax-custom_hscode",
+                    "Item Tax-custom_tims_hscode",
                     "Item Tax-custom_section_break_k5uid",
                     "Item Tax-custom_description",
                 ),
